d3775a8f00e42f961af825fad3cc9134ea7fe757
Estefania A <zestefania.amundaray@gmail.com>
2022-08-16 18:01:23 +0200

    Exercises done-no routing

---
+++ my-first-project/src/components/ShowMessage.vue
@@ -0,0 +1,27 @@
+<template>
+   <div>
+        <h1>Dynamic Message:{{msg}} </h1>
+        <button @click="showConsole">console</button>
+   </div> 
+</template>
+
+<script>
+import { ref, defineComponent } from "vue";
+export default {
+    name:"ShowMessage",
+    setup: () => {
+    const msg = ref("initial message");
+ 
+    const showConsole = () => {
+      console.log("Hello World");
+    }
+ 
+    return { msg, showConsole };
+
+}
+}
+</script>
+
+<style>
+
+</style>
